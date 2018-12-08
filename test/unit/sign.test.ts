456d8dfabe874644b9cfeffbe3a861244fd3417f
WMXPY <wm@mengw.io>
2018-12-07 19:57:16 -0600

    add token sign uni test

--- test/unit/sign.test.ts
+++ test/unit/sign.test.ts
@@ -14,7 +14,7 @@ describe('Given {BrontosaurusSign} class', (): void => {
 
     const chance: Chance.Chance = new Chance('brontosaurus-sign');
 
-    it('should be able to generate token', (): void => {
+    it('should be able to construct object', (): void => {
 
         const secret: string = chance.string();
         const mockObject: MockEncryptableObject = MockEncryptableObject.create();
@@ -22,5 +22,16 @@ describe('Given {BrontosaurusSign} class', (): void => {
         const sign: BrontosaurusSign = BrontosaurusSign.create(mockObject.object, secret);
 
         expect(sign.object).to.be.deep.equal(mockObject.object);
+    });
+
+    it('should be able to generate token', (): void => {
+
+        const secret: string = chance.string();
+        const mockObject: MockEncryptableObject = MockEncryptableObject.create();
+
+        const sign: BrontosaurusSign = BrontosaurusSign.create(mockObject.object, secret);
+        const token: string = sign.token();
+
+        expect(token).to.be.equal(sign.token());
     });
 });
