f5c952895d0e19061791d16fab5ff2e70c95ddc0
PCXPY <pc@wmpcxpy.com>
2018-12-21 11:17:01 -0600

    [Test] Implement unit test for token class

---
+++ test/unit/token.test.ts
@@ -0,0 +1,89 @@
+/**
+ * @author WMXPY
+ * @namespace Brontosaurus
+ * @description Token
+ * @package Unit Test
+ */
+
+import { expect } from 'chai';
+import * as Chance from 'chance';
+import { BrontosaurusSign } from '../../src/sign';
+import { BrontosaurusToken } from '../../src/token';
+import { MockEncryptableObject } from '../mock/encryptable-object';
+
+describe('Given {BrontosaurusToken} class', (): void => {
+
+    const chance: Chance.Chance = new Chance('brontosaurus-token');
+
+    it('should be able to construct object', (): void => {
+
+        const secret: string = chance.string();
+        const token: BrontosaurusToken = BrontosaurusToken.withSecret(secret);
+
+        expect(token).to.be.instanceOf(BrontosaurusToken);
+    });
+
+    it('should be valid if valid', (): void => {
+
+        const currentTime: number = Date.now();
+
+        const secret: string = chance.string();
+        const mockObject: MockEncryptableObject = MockEncryptableObject.create();
+
+        const sign: BrontosaurusSign = BrontosaurusSign.create(mockObject.object, secret);
+        const token: string = sign.token(currentTime);
+
+        const clazz: BrontosaurusToken = BrontosaurusToken.withSecret(secret);
+
+        // tslint:disable-next-line
+        expect(clazz.validate(token, 5000)).to.be.true;
+    });
+
+    it('should be invalid if expired', (): void => {
+
+        const mockTime: number = Date.now() - 8000;
+
+        const secret: string = chance.string();
+        const mockObject: MockEncryptableObject = MockEncryptableObject.create();
+
+        const sign: BrontosaurusSign = BrontosaurusSign.create(mockObject.object, secret);
+        const token: string = sign.token(mockTime);
+
+        const clazz: BrontosaurusToken = BrontosaurusToken.withSecret(secret);
+
+        // tslint:disable-next-line
+        expect(clazz.validate(token, 5000)).to.be.false;
+    });
+
+    it('should be invalid if time traveling', (): void => {
+
+        const mockTime: number = Date.now() + 8000;
+
+        const secret: string = chance.string();
+        const mockObject: MockEncryptableObject = MockEncryptableObject.create();
+
+        const sign: BrontosaurusSign = BrontosaurusSign.create(mockObject.object, secret);
+        const token: string = sign.token(mockTime);
+
+        const clazz: BrontosaurusToken = BrontosaurusToken.withSecret(secret);
+
+        // tslint:disable-next-line
+        expect(clazz.validate(token, 5000)).to.be.false;
+    });
+
+    it('should be invalid if can not crypt', (): void => {
+
+        const mockTime: number = Date.now();
+
+        const secret: string = chance.string();
+        const mockObject: MockEncryptableObject = MockEncryptableObject.create();
+
+        const sign: BrontosaurusSign = BrontosaurusSign.create(mockObject.object, secret);
+        const token: string = sign.token(mockTime) + chance.string();
+
+        const clazz: BrontosaurusToken = BrontosaurusToken.withSecret(secret);
+
+        // tslint:disable-next-line
+        expect(clazz.validate(token, 5000)).to.be.false;
+    });
+});
