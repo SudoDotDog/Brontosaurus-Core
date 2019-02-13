f34999f63bdbc57ff446a5de088cb0f7bf9b162a
WMXPY <wm@mengw.io>
2019-02-13 17:07:29 -0600

    fix unit test

--- test/mock/token.ts
+++ test/mock/token.ts
@@ -9,9 +9,11 @@ import * as Chance from 'chance';
 
 const chance: Chance.Chance = new Chance('mock-body');
 
-export const createMockBody = (username: string = chance.string(), groups: [] = [], infos: {} = {}): IBrontosaurusBody => ({
+export const createMockBody = (username: string = chance.string(), groups: [] = [], infos: {} = {}, beacons: {} = {}, mint: string = ""): IBrontosaurusBody => ({
 
     username,
     groups,
     infos,
+    beacons,
+    mint,
 });
